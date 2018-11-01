81b6a5346f28d2f2dfdefbaadd910f29ded2ad45
BQ Lab <beaqon@naver.com>
2018-11-01 14:31:39 +0900

    코드상으로는 구현끝

--- app/src/main/java/com/example/jyseo/han3/TabHostActivity.java
+++ app/src/main/java/com/example/jyseo/han3/TabHostActivity.java
@@ -1,7 +1,11 @@
 package com.example.jyseo.han3;
 
 import android.Manifest;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -14,6 +18,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothService;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class TabHostActivity extends AppCompatActivity implements View.OnClickListener, Runnable {
     private final int L1 = 1;
@@ -30,13 +39,19 @@ public class TabHostActivity extends AppCompatActivity implements View.OnClickLi
     private Layout3 l3;
 
     private boolean isNoticed = false;
+    private boolean isConnected = false;
+    private BluetoothSPP bluetoothSPP;
     private String phone = "01062040454";
     private String message = "테스트 오지게 박습니다 행님!";
+
+    public int hartrate;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap_host);
         init();
+        setBluetoothSPP();
+        connectBluetooth();
     }
 
     @Override
@@ -60,11 +75,15 @@ public class TabHostActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void run() {
-        while (BluetoothService.isConnected) {
+        while(isConnected) {
             try {
                 Thread.sleep(1000);
-                l1.setHartrate(BluetoothService.hartrate);
-                checkHartrate(BluetoothService.hartrate);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        checkHartrate();
+                    }
+                });
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -89,7 +108,6 @@ public class TabHostActivity extends AppCompatActivity implements View.OnClickLi
 
         // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
         callLayout(L2);
-        startService();
     }
 
     private void callLayout(int layout_no) {
@@ -108,12 +126,72 @@ public class TabHostActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    public void checkHartrate(int hartrate) {
+    public void setBluetoothSPP() {
+        bluetoothSPP = new BluetoothSPP(this);
+        if (!bluetoothSPP.isBluetoothAvailable()) {
+            Toast.makeText(this, "블루투스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show();
+            finishAffinity();
+        }
+        bluetoothSPP.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            @Override
+            public void onDataReceived(byte[] data, String message) {
+                TabHostActivity.this.hartrate = Integer.parseInt(message);
+            }
+        });
+        bluetoothSPP.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            @Override
+            public void onDeviceConnected(String name, String address) {
+                Toast.makeText(TabHostActivity.this, "디바이스와 연결되었습니다.", Toast.LENGTH_SHORT).show();
+                isConnected = true;
+            }
+
+            @Override
+            public void onDeviceDisconnected() {
+                Toast.makeText(TabHostActivity.this, "디바이스와의 연결이 끊겼습니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDeviceConnectionFailed() {
+                Toast.makeText(TabHostActivity.this, "디바이스와 연결할 수 없습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void connectBluetooth() {
+        if (!bluetoothSPP.isBluetoothEnabled()) //핸드폰에서 블루투스가 꺼져 있다면
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), BluetoothState.REQUEST_ENABLE_BT);
+        else if(!bluetoothSPP.isServiceAvailable()) {
+            bluetoothSPP.setupService();
+            bluetoothSPP.startService(BluetoothState.DEVICE_OTHER); //안드로이드 기기가 아닌 아두이노 같은 장치도 연결이 가능하게 서비스 설정
+        }
+        if (bluetoothSPP.getServiceState() == BluetoothState.STATE_CONNECTED)
+            bluetoothSPP.disconnect();
+        else //디바이스리스트는 연결 가능한 디바이스 리스트를 의미하며, 이 리스트에 HC-06이 없으면 페어링이 되어있지 않은 겁니다.
+            startActivityForResult(new Intent(getApplicationContext(), DeviceList.class), BluetoothState.REQUEST_CONNECT_DEVICE);
+    }
+
+    public void checkHartrate() {
         if ((hartrate < 40 || hartrate > 140) && !isNoticed) {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phone, null, message, null, null);
         } else if (!(hartrate < 40 || hartrate > 140) && isNoticed)
             isNoticed = false;
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
+                bluetoothSPP.connect(data);
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bluetoothSPP.setupService();
+                bluetoothSPP.startService(BluetoothState.DEVICE_OTHER);
+            } else {
+                Toast.makeText(this,"블루투스가 비활성화되어 있습니다.", Toast.LENGTH_LONG).show();
+                finishAffinity();
+            }
+        }
+    }
+
 }
 
